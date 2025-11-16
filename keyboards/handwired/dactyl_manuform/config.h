4424cb485fcf092bdec16d32017e05ece246b388
d <dsandall@calpoly.edu>
2025-11-16 11:45:48 -0800

    debounce fix(?)

--- keyboards/handwired/dactyl_manuform/config.h
+++ keyboards/handwired/dactyl_manuform/config.h
@@ -18,13 +18,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-
 /* mouse config */
-#define MOUSEKEY_INTERVAL    20
-#define MOUSEKEY_DELAY       0
+#define MOUSEKEY_INTERVAL 20
+#define MOUSEKEY_DELAY 0
 #define MOUSEKEY_TIME_TO_MAX 60
-#define MOUSEKEY_MAX_SPEED   7
+#define MOUSEKEY_MAX_SPEED 7
 #define MOUSEKEY_WHEEL_DELAY 0
 
 /* Enables This makes it easier for fast typists to use dual-function keys */
 #define PERMISSIVE_HOLD
+
+// NOTE: I needed this to fix some debounce issues that popped up due to shit wirring <- see there it is
+#define DEBOUNCE 10
